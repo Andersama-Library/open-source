dc6d02655519201d9a726c0a0afa35e6d4de13ce
Andersama <anderson.john.alexander@gmail.com>
2024-06-22 16:07:58 -0700

    chore: enclose in fnv1a namespace

--- fnv1a/fnv1a.h
+++ fnv1a/fnv1a.h
@@ -1,26 +1,28 @@
 #pragma once
 #include <cstdint>
- 
-constexpr size_t fnv1a(const uint8_t* ptr, size_t count) noexcept {
-	size_t hash = 0xcbf29ce484222325ull;
 
-	constexpr size_t fnv_prime = 0x00000100000001B3ull;
-
-	for (size_t i = 0; i < count; i++) {
-		hash = (ptr[i] ^ hash) * fnv_prime;
+namespace fnv1a {
+	constexpr size_t fnv1a(const uint8_t* ptr, size_t count) noexcept {
+		size_t hash = 0xcbf29ce484222325ull;
+	
+		constexpr size_t fnv_prime = 0x00000100000001B3ull;
+	
+		for (size_t i = 0; i < count; i++) {
+			hash = (ptr[i] ^ hash) * fnv_prime;
+		}
+	
+		return hash;
 	}
-
-	return hash;
-}
-
-constexpr uint32_t fnv1a_32(const uint8_t* ptr, size_t count) noexcept {
-	uint32_t hash = 0x811c9dc5;
-
-	constexpr uint32_t fnv_prime = 0x01000193;
-
-	for (size_t i = 0; i < count; i++) {
-		hash = (ptr[i] ^ hash) * fnv_prime;
+	
+	constexpr uint32_t fnv1a_32(const uint8_t* ptr, size_t count) noexcept {
+		uint32_t hash = 0x811c9dc5;
+	
+		constexpr uint32_t fnv_prime = 0x01000193;
+	
+		for (size_t i = 0; i < count; i++) {
+			hash = (ptr[i] ^ hash) * fnv_prime;
+		}
+	
+		return hash;
 	}
-
-	return hash;
 }
