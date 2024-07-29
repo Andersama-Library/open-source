4ca71c0f9707e2c5ca69aad5eacd631a1350bd6e
Andersama <anderson.john.alexander@gmail.com>
2024-07-29 07:03:28 -0700

    fixup: remove _NODISCARD use [[nodiscard]]

--- sort/sort.h
+++ sort/sort.h
@@ -286,7 +286,7 @@ namespace sort {
 	// Wrappers so we can expand > and < support inside namespace sort vs namespace std (which can be UB)
 	template<> struct less<void> {
 		template<class T0, class T1>
-		_NODISCARD constexpr auto operator()(T0&& lhs, T1&& rhs) const
+		[[nodiscard]] constexpr auto operator()(T0&& lhs, T1&& rhs) const
 						noexcept(noexcept(static_cast<T0&&>(lhs) < static_cast<T1&&>(rhs))) // strengthened
 						-> decltype(static_cast<T0&&>(lhs) < static_cast<T1&&>(rhs))
 		{
@@ -308,7 +308,7 @@ namespace sort {
 
 	template<> struct greater<void> {
 		template<class T0, class T1>
-		_NODISCARD constexpr auto operator()(T0&& lhs, T1&& rhs) const
+		[[nodiscard]] constexpr auto operator()(T0&& lhs, T1&& rhs) const
 						noexcept(noexcept(static_cast<T0&&>(lhs) < static_cast<T1&&>(rhs))) // strengthened
 						-> decltype(static_cast<T0&&>(lhs) < static_cast<T1&&>(rhs))
 		{
